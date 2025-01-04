dbcd0cc5e681ce8a0a75e6ac0cb90cb239348191
Zhiyun Qin <96156298+Okita1027@users.noreply.github.com>
2025-01-04 22:52:55 +0800

    SPI扩展：序列化工厂懒加载

--- qzy-rpc-core/src/main/java/learn/qzy/rpc/spi/SpiLoader.java
+++ qzy-rpc-core/src/main/java/learn/qzy/rpc/spi/SpiLoader.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
@@ -27,12 +28,12 @@ public class SpiLoader {
     /**
      * 存储已加载的类：接口名 => (key => 实现类)
      */
-    private static Map<String, Map<String, Class<?>>> loaderMap = new ConcurrentHashMap<>();
+    private static final Map<String, Map<String, Class<?>>> loaderMap = new ConcurrentHashMap<>();
 
     /**
      * 对象实例缓存（避免重复new），类路径 => 对象实例，单例模式
      */
-    private static Map<String, Object> instanceCache = new ConcurrentHashMap<>();
+    private static final Map<String, Object> instanceCache = new ConcurrentHashMap<>();
 
     /**
      * 系统 SPI 目录
@@ -67,7 +68,7 @@ public class SpiLoader {
     /**
      * 加载某个类型
      */
-    public static Map<String, Class<?>> load(Class<?> loadClass) {
+    public static void load(Class<?> loadClass) {
         log.info("加载类型为 {} 的 SPI", loadClass.getName());
         // 扫描路径，用户自定义的 SPI 优先级高于系统 SPI
         Map<String, Class<?>> keyClassMap = new HashMap<>();
@@ -75,9 +76,8 @@ public class SpiLoader {
             List<URL> resources = ResourceUtil.getResources(scanDir + loadClass.getName());
             // 读取每个资源
             for (URL resource : resources) {
-                try {
-                    InputStreamReader inputStreamReader = new InputStreamReader(resource.openStream());
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                try (InputStreamReader inputStreamReader = new InputStreamReader(resource.openStream());
+                     BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
                     String line;
                     while ((line = bufferedReader.readLine()) != null) {
                         String[] strArray = line.split("=");
@@ -93,13 +93,19 @@ public class SpiLoader {
             }
         }
         loaderMap.put(loadClass.getName(), keyClassMap);
-        return keyClassMap;
     }
 
     /**
      * 获取某个接口的实例
      */
     public static <T> T getInstance(Class<T> tClass, String key) {
+        if (loaderMap.isEmpty()) {
+            synchronized (loaderMap) {
+                if (loaderMap.isEmpty()) {
+                    load(Serializer.class);
+                }
+            }
+        }
         String tClassName = tClass.getName();
         Map<String, Class<?>> keyClassMap = loaderMap.get(tClassName);
         if (keyClassMap == null) {
@@ -114,10 +120,12 @@ public class SpiLoader {
         String implClassName = implClass.getName();
         if (!instanceCache.containsKey(implClassName)) {
             try {
-                instanceCache.put(implClassName, implClass.newInstance());
+                instanceCache.put(implClassName, implClass.getConstructor().newInstance());
             } catch (InstantiationException | IllegalAccessException e) {
                 String errorMsg = String.format("%s 类实例化失败", implClassName);
                 throw new RuntimeException(errorMsg, e);
+            } catch (InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
             }
         }
         return (T) instanceCache.get(implClassName);
